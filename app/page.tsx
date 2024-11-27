04591fe60ff023ef61d9c4f82a5b268a0ac272eb
Liam Meyer <liam.meyer@extend.app>
2024-11-27 15:12:59 -0600

    added logo

--- app/page.tsx
+++ app/page.tsx
@@ -19,6 +19,7 @@ export default function Home() {
             By{' '}
             <Image
               src="/vercel.svg"
+              // "/mini_logo.png"
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
@@ -32,16 +33,16 @@ export default function Home() {
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
         <a
           className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
+          href="https://www.nytimes.com/crosswords/game/mini"
+          // target="_blank"
           rel="noopener noreferrer"
         >
           <Image
             className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
+            src="/mini_logo.png"
+            alt="Link to today's mini"
+            width={50}
+            height={50}
             priority
           />
         </a>
