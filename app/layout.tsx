4ead85deb77b396ffeaf64c345cd0a01faf2e2a6
Liam Meyer <liam.meyer@extend.app>
2025-01-16 11:04:27 -0500

    added favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,19 @@ import { ThemeProvider } from './ThemeContext'; // Import ThemeProvider
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'NYT Mini Archive',
+  title: 'Mini Archive',
   description: 'Created by Liam Meyer',
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png' },
+    ],
+  },
+  manifest: '/site.webmanifest',
 };
 
 export default function RootLayout({
