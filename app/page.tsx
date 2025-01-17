8b8b893e25d0b514368b05cc45dcf9f2455b1c4c
Liam Meyer <liam.meyer@extend.app>
2025-01-17 09:11:38 -0500

    added margin to bottom to help mobile

--- app/page.tsx
+++ app/page.tsx
@@ -354,7 +354,7 @@ export default function Home() {
       {/* Random Puzzle Button */}
       <div>
         <button
-          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 mb-10"
           onClick={getRandomUncompletedDate}
         >
           Go to a Random Uncompleted Puzzle
